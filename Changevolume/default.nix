e142b4124b59254b212681657acb1976759d50c0
krovuxdev <62192487+krovuxdev@users.noreply.github.com>
2024-03-25 18:45:28 -0500

    idk

--- Changevolume/default.nix
+++ Changevolume/default.nix
@@ -1,7 +1,12 @@
-{ mkDerivation, ... }:
-mkDerivation {
-  pname = "changevolume";
+{pkgs}:
+pkgs.stdenv.mkDerivation {
+  name = "changevolume";
   version = "1.0";
-  src = ./changevolume.nix;
+  src = ./.;
+  #buildInputs = [(import ./changevolume.nix {})];
+  meta = with pkgs.lib; {
+    description = "My Changevolume is Control Volume";
+    license = licenses.mit;
+    platforms = platforms.all;
+  };
 }
-
--- flake.nix
+++ flake.nix
@@ -1,14 +1,47 @@
 {
   description = "Proyecto changevolume";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs, ... }: {
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    inherit (nixpkgs) lib;
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    eachSystems = lib.genAttrs systems;
+    forSystem = eachSystems (
+      system: let
+        Pkgs = import nixpkgs {inherit system;};
+      in
+        Pkgs
+        // {
+          changevolume = import ./Changevolume {
+            inherit system;
+            pkgs = Pkgs;
+          };
+        }
+    );
+  in {
+    packages = eachSystems (system: {
+      changevolume = import ./Changevolume/default.nix {
+        #inherit system;
+        pkgs = (system: import nixpkgs {inherit system;});
+      };
+      #eachSystems.${system}.changevolumee
+    });
+    homeManagerModules = eachSystems (system: {
+      changevolume = import ./Changevolume/changevolume.nix self;
+    });
     nixosModules = {
-        changevolume = import ./Changevolume/changevolume.nix;
-        home-manager = import ./Changevolume/changevolume.nix ;
-        };
+    changevolume = import ./Changevolume/changevolume.nix self;
+    };
+    checks = eachSystems (system: self.packages.${system});
+    formatter = eachSystems (system: forSystem.${system}.alejandra);
   };
 }
